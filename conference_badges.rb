ab6d41edcadfc1684d384a92b4095ab6e30c4be1
Henna Vahatiitto <hnnamv@gmail.com>
2019-12-19 09:04:14 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,18 +1,27 @@
 def badge_maker(name)
-  puts "Hello, my name is #{name}."
+  "Hello, my name is #{name}."
 end
 
-badge_maker
+def batch_badge_creator(attendees)
+  attendees.collect do |name|
+    badge_maker(name)
+  end
 
-def batch_badge_creator
-  names["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
-  names.each do |name|
-    puts "Hello, my name is #{name}"
 end
 
-batch_badge_creator
+def assign_rooms(attendees)
+  room=0
+  attendees.collect do |name|
+    room+=1
+    "Hello, #{name}! You'll be assigned to room #{room}!"
+  end
+end
 
-def assign_rooms
-  names["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
-  rooms[1,2,3,4,5,6,7]
-  
+def printer(attendees)
+  batch_badge_creator(attendees).each do |value|
+    puts value
+  end
+  assign_rooms(attendees).each do |value|
+    puts value
+  end
+end
