ec7609a5f1ce1f44ade5e3facc0d9388aa300405
Henna Vahatiitto <hnnamv@gmail.com>
2019-12-19 09:00:52 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,10 +4,15 @@ end
 
 badge_maker
 
-def batch_badge_creator(names)
+def batch_badge_creator
   names["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
   names.each do |name|
     puts "Hello, my name is #{name}"
 end
 
 batch_badge_creator
+
+def assign_rooms
+  names["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+  rooms[1,2,3,4,5,6,7]
+  
