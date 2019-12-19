e66dffec1d9d02163ec8686543e16e588239c17c
Henna Vahatiitto <hnnamv@gmail.com>
2019-12-19 08:56:05 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,5 +2,10 @@ def badge_maker(name)
   puts "Hello, my name is #{name}."
 end
 
+badge_maker
+
 def batch_badge_creator(names)
-  names[]
+  names["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+  names.each do |name|
+    puts "Hello, my name is #{name}"
+end
